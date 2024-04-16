1b75fc21626780c538202c50b4f05547f28f4c43
Christian Johannsen <christian.johannsen@chainguard.dev>
2024-04-16 10:13:46 -0500

    adding python examples

---
+++ inky/inky.py
@@ -0,0 +1,12 @@
+'''import climage module to display images on terminal'''
+from climage import convert
+
+
+def main():
+    '''Take in PNG and output as ANSI to terminal'''
+    output = convert('inky.png', is_unicode=True)
+    print(output)
+
+if __name__ == "__main__":
+    main()
+
